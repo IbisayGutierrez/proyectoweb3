c2afe1d5c7bb4a46f37f47b3e0939c7fae11897d
Chrishg4 <chhernandezga@est.utn.ac.cr>
2025-12-02 00:45:26 -0600

    Creacion de la ruta login y demas verificacion por token
    Middleware de inicio de sesión, middleware basado en roles, rutas de inicio de sesión y servicio de inicio de sesión. Se actualizó .env.example para incluir variables de configuración JWT. Se agregó una utilidad de registro para el registro de eventos.

---
+++ server/services/loginService.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import { validarUsuario } from './usuariosService';
+
+export const login = async (correo, contrasena) => {
+    const usuario = await validarUsuario(correo, contrasena);
+    if (!usuario) {
+        throw new Error('Credenciales inválidas');
+    }
+    const token = jwt.sign(
+        {
+            id: usuario.id,
+            correo: usuario.correo,
+            rol: usuario.rol
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: Number(process.env.JWT_EXPIRES_IN) }
+    );
+    return {token , usuario};
+};
